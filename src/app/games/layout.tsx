ca0b4e4f1f87e677367da901013f760243c29cdc
Lincoln Ruteski <lruteski@gmail.com>
2024-11-30 17:42:59 -0300

    Construindo a navbar

--- src/app/games/layout.tsx
+++ src/app/games/layout.tsx
@@ -2,6 +2,8 @@ export default function GamesLayout({children}: {children: React.ReactNode}) {
   return (
     <div>
       <p>Games Layout</p>
+      <button className="px-4 py-2 rounded-md bg-indigo-500 text-white hover:bg-indigo-600">Save</button>
+
       {children}
     </div>
   )
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+export const Navbar= ()=> {
+  return (
+                                                                            // 0 /40 é opacidade
+    <nav className="flex h-screen flex-col gap-4 bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2">
+      <div><img className="max-w-full p-2" src="http://www.jfmoita.pt/wp-content/uploads/2024/03/Brasao_Moita_Pequeno-1.png" alt=""/></div>
+      <ul className="flex-grow my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Home</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Games</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Top 10</li>
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">Walkthroughs</li>
+      </ul>
+      <ul className="my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
+        <li className="my-2 rounded-lg bg-transparent p-2 hover:bg-slate-800 cursor-pointer">User</li>
+      </ul>
+    </nav>
+  )
+}
