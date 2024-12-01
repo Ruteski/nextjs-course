bf43ef9855fc0407ddf20e742c0fe2f493a28433
Lincoln Ruteski <lruteski@gmail.com>
2024-12-01 12:29:22 -0300

    Consultando artigos no banco de dados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import articles from '@/data/articles.json'
 import Image from 'next/image';
+import Article from './libs/database/Articles';
 
-export default function Home() {
+export default async function Home() {
+
+  // const highlightedArticles = await Article.get({limit: 4});
+  // const articles = await Article.get({limit: 10, offset: 4});
+
+  const articles = await Article.get({
+    orderBy: {publishedAt: 'desc'},
+    limit: 14
+  });
+  const highlightedArticles = articles.slice(0,4)
+  const listArticles = articles.slice(4)
+  
+
+  console.log('*** ~ Home articles:', articles);
+  
+
   return (
     <div className="ml-72">
       <div className="w-full h-[35vh] bg-orange-400 flex-center">
@@ -10,7 +25,7 @@ export default function Home() {
 
       <div className="container mx-auto my-6">
         <div className="grid grid-cols-4 gap-4 h-[35vh]">
-          {articles.splice(-4).map((article) => {
+          {highlightedArticles.map((article) => {
             return (
               <div 
                 key={article.title} 
@@ -20,12 +35,13 @@ export default function Home() {
                 <Image 
                   className='h-full w-full object-cover transition duration-500 hover:scale-105 rounded-r-lg'
                   src={`/assets/images/articles/${article.image}`} 
-                  alt={article.image_description} 
+                  alt={article.title} 
                   width={600} 
                   height={400} 
                 />
                 </div>
                 <p className='absolute bottom-0 pt-6 pb-2 px-2 bg-gradient-to-t from-slate-900 via-slate-800 to-transparent w-full'>
+                  {/* {article.id} - {article.title} */}
                   {article.title}
                 </p>
               </div>
@@ -38,7 +54,8 @@ export default function Home() {
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-8  flex flex-col gap-4">
 
-            {articles.map((article) => {
+            {/* {articles.map((article) => { */}
+            {listArticles.map((article) => {
               return (
                 <div key={article.title} className="flex bg-slate-800 rounded-md py-4">
                   <div className='flex items-center'>
@@ -46,18 +63,20 @@ export default function Home() {
                       <Image 
                         className='h-full w-full object-cover transition duration-500 hover:scale-105 rounded-r-lg'
                         src={`/assets/images/articles/${article.image}`} 
-                        alt={article.image_description} 
+                        alt={article.title} 
                         width={600} 
                         height={400} 
                       />
                     </div>
                   </div>
                   <div className="w-full flex flex-col gap-2 pl-4">
+                    {/* <h2 className="text-3xl mb-4 text-indigo-400/90">{article.id} - {article.title}</h2> */}
                     <h2 className="text-3xl mb-4 text-indigo-400/90">{article.title}</h2>
+                    {/* <p className="flex-grow">{article.publishedAt.toISOString()}</p> */}
                     <p className="flex-grow">{article.excerpt}</p>
                     <button className='bg-slate-700 hover:bg-indigo-400/40 rounded-lg px-4 py-2 inline max-w-max'>
                       Ler mais
-                      </button>
+                    </button>
                   </div>
                 </div>
               )
