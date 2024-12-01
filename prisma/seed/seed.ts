97a76ea13e90a17df75ed2385fd6076ef88aa86a
Lincoln Ruteski <lruteski@gmail.com>
2024-12-01 11:58:28 -0300

    Importando os articles para o banco de dados

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,42 @@
 import {PrismaClient} from '@prisma/client'
 
+import articles from '../../src/data/articles.json'
+import { slugfy } from '../../src/helpers/slugfy';
+
 const prisma = new PrismaClient()
+const isDev = process.env.NODE_ENV === "development"
 
 async function main() {
-  console.log('db seed');
+  console.log('Run seed');
+  seedArticles()
+}
+
+async function seedArticles() {
+  if (!isDev) {
+    throw new Error("NODE_ENV is not a development environment")
+  }
+
+  // deleta todos os registros
+  await prisma.article.deleteMany()
+  
+  // fazendo o truncate nas sequences da table article, roda comando direto no banco
+  await prisma.$executeRawUnsafe("DELETE FROM SQLITE_SEQUENCE WHERE name=$1", "Article")
+  
+  for(const article of articles) {
+    const record = await prisma.article.create({
+      data: {
+        title: article.title,
+        slug: slugfy(article.title),
+        excerpt: article.excerpt,
+        content: article.content,
+        image: article.image,
+        publishedAt: new Date(article.publish_date),
+      }
+    })
+
+    console.log('*** created article', record.id, record.title);
+    
+  }
 }
 
 main().then(async() => {
